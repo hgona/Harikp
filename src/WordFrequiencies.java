f849e11efd079a4e76873caafc83ee7b118574e4
gonah <ghkr1212@gmail.com>
2017-10-12 11:19:42 -0400

    no message

--- src/WordFrequiencies.java
+++ src/WordFrequiencies.java
@@ -1,7 +1,48 @@
-package PACKAGE_NAME;
-
-/**
- * Created by gonah on 10/12/2017.
- */
+import edu.duke.*;
+import java.util.ArrayList;
 public class WordFrequiencies {
-}
+        private ArrayList<String> myWords;
+        private ArrayList<Integer> myFreqs;
+    public WordFrequiencies() {
+            myWords = new ArrayList<String>();
+            myFreqs = new ArrayList<Integer>();
+        }
+    public void findUnique(){
+            FileResource resource = new FileResource();
+
+            for(String s : resource.words()){
+                s = s.toLowerCase();
+                int index = myWords.indexOf(s);
+                if (index == -1){
+                    myWords.add(s);
+                    myFreqs.add(1);
+                }
+                else {
+                    int freq = myFreqs.get(index);
+                    myFreqs.set(index,freq+1);
+                }
+            }
+        }
+
+        public void tester(){
+            findUnique();
+            System.out.println("# unique words: "+myWords.size());
+            for (int i = 0; i < myWords.size(); i++) {
+                System.out.println(myFreqs.get(i)+":"+myWords.get(i));
+            }
+            int index = findMax();
+            System.out.println("max word/freq: "+myWords.get(index)+" "+myFreqs.get(index));
+        }
+        public int findMax(){
+            int max = myFreqs.get(0);
+            int maxIndex = 0;
+            for(int k=0; k < myFreqs.size(); k++){
+                if (myFreqs.get(k) > max){
+                    max = myFreqs.get(k);
+                    maxIndex = k;
+                }
+            }
+            return maxIndex;
+        }
+    }
+
